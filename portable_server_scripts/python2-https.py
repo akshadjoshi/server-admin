8a94a10a40e8b82986cf280469f57e5f3a3d07a6
akshadjoshi <106912619+akshadjoshi@users.noreply.github.com>
2023-10-03 13:13:53 +0530

    Create python2-https.py

---
+++ portable_server_scripts/python2-https.py
@@ -0,0 +1,12 @@
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0',443),
+        SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+httpd.socket = ssl.wrap_socket (httpd.socket,
+        keyfile="key.pem",
+        certfile='cert.pem', server_side=True)
+
+httpd.serve_forever()
