541eff3fc38314ad2bbebc1f87537ea21e537cdb
akshadjoshi <106912619+akshadjoshi@users.noreply.github.com>
2023-10-04 12:49:57 +0530

    Update tftp-server.py

--- portable_server_scripts/tftp-server.py
+++ portable_server_scripts/tftp-server.py
@@ -1,7 +1,7 @@
 #pip3 install tftpy
 from tftpy import TftpServer
 
-server = TftpServer('/opt/transfer')
+server = TftpServer('/opt/transfer') # you need to change the share location depending on dir you are running this server
 
 try:
     server.listen('0.0.0.0', 69)
