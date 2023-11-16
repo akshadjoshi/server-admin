cb59df2079e81b74a5dd2f96cc97d791f7425da6
akshadjoshi <106912619+akshadjoshi@users.noreply.github.com>
2023-11-16 19:34:17 +0530

    Delete portable_server_scripts directory

--- portable_server_scripts/tftp-server.py
+++
@@ -1,9 +0,0 @@
-#pip3 install tftpy
-from tftpy import TftpServer
-
-server = TftpServer('/opt/transfer') # you need to change the share location depending on dir you are running this server
-
-try:
-    server.listen('0.0.0.0', 69)
-except KeyboardInterrupt:
-    pass
